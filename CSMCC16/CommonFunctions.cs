67d446805d899ba98942044db4d18aff165217f2
Gareth Hutchins <gareth.hutchins@gmail.com>
2019-01-29 14:59:29 -0800

    Add Passegner Tuple and first format checks

--- CSMCC16/CommonFunctions.cs
+++ CSMCC16/CommonFunctions.cs
@@ -4,51 +4,39 @@ using System.Windows.Forms;
 using System.Threading.Tasks;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public class Mapper
 {
     public string PassengerFile = "";
     public TextBox log;
-    public object x = new List<KeyValuePair<string, object>>();
-    
-    private KeyValuePair<string, object> p = new KeyValuePair<string, object>();
-   private class PassengerData
-    {
-        string passengerID;
-        string flightID;
-        string srcApt;
-        string dstApt;
-        DateTime dptTime;
-        DateTime flghtDur;
-    }
-    private class AirportData
-    {
-        string aptName;
-        string aptCode;
-        int lat;
-        int lon;
-    }
-    private class ErrorPassenger
-    {
-        string InputCSV;
-        string Error;
-    }
-    private class ErrorAirpot
-    {
-        string InputCSV;
-        string Error;
-    }
+    public List<Tuple<string,object>> Passengers = new List<Tuple<string,object>>();
+    public List<Tuple<string,string>> PassengerErrors = new List<Tuple<string,string>>();
+   
     public void Map()
     {
         log.AppendText(System.Environment.NewLine + "Opening Passenger File" + PassengerFile);
-        //Create passenger Array
-        PassengerData[] passengers = new PassengerData[0];
+        //Set the format checking Regexs
+        //Passenger ID
+        Regex PID = new Regex("[A-Z]{3}[0-9]{4}[A-Z]{2}[0-9]{1}");
         Parallel.ForEach(File.ReadLines(PassengerFile).Select(line => line.Split(',')),
             components =>
             {
                 Boolean OK = true;
                 string ErrorText = "";
-               
+                //Get the first column
+                
+                //check the format is OK
+                
+                if (!PID.IsMatch(components[0])) {
+                    OK = false;
+                    PassengerErrors.Add(new Tuple<string, string>("Invalid Passgenger ID"),components[0]);
+                }
+                else {
+                    Passengers.Add(new Tuple<string, object>("PassangerID",components[0]));
+                }
+                
+              
             }
             );
            
