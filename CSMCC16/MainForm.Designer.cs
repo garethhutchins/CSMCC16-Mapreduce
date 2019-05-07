911c961f1f4b5ea9a46769ca82a9c8d95b61afc8
Gareth Hutchins <Gareth.Hutchins@gmail.com>
2019-05-07 21:18:25 +0100

    Added Shortcuts and Tab Orders

--- CSMCC16/MainForm.Designer.cs
+++ CSMCC16/MainForm.Designer.cs
@@ -75,9 +75,9 @@
             // 
             this.btn_output.Location = new System.Drawing.Point(406, 92);
             this.btn_output.Name = "btn_output";
-            this.btn_output.Size = new System.Drawing.Size(75, 23);
-            this.btn_output.TabIndex = 8;
-            this.btn_output.Text = "Browse";
+            this.btn_output.Size = new System.Drawing.Size(114, 23);
+            this.btn_output.TabIndex = 5;
+            this.btn_output.Text = "&Output Directory";
             this.btn_output.UseVisualStyleBackColor = true;
             this.btn_output.Click += new System.EventHandler(this.btn_output_Click);
             // 
@@ -95,15 +95,15 @@
             this.txt_output.Location = new System.Drawing.Point(117, 96);
             this.txt_output.Name = "txt_output";
             this.txt_output.Size = new System.Drawing.Size(282, 20);
-            this.txt_output.TabIndex = 6;
+            this.txt_output.TabIndex = 4;
             // 
             // btn_apt
             // 
             this.btn_apt.Location = new System.Drawing.Point(405, 50);
             this.btn_apt.Name = "btn_apt";
-            this.btn_apt.Size = new System.Drawing.Size(75, 23);
-            this.btn_apt.TabIndex = 5;
-            this.btn_apt.Text = "Browse";
+            this.btn_apt.Size = new System.Drawing.Size(115, 23);
+            this.btn_apt.TabIndex = 3;
+            this.btn_apt.Text = "&Airports Browse";
             this.btn_apt.UseVisualStyleBackColor = true;
             this.btn_apt.Click += new System.EventHandler(this.btn_apt_Click);
             // 
@@ -115,22 +115,21 @@
             this.label2.Size = new System.Drawing.Size(82, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Airport Data File";
-            
             // 
             // txt_apt
             // 
             this.txt_apt.Location = new System.Drawing.Point(116, 54);
             this.txt_apt.Name = "txt_apt";
             this.txt_apt.Size = new System.Drawing.Size(282, 20);
-            this.txt_apt.TabIndex = 3;
+            this.txt_apt.TabIndex = 2;
             // 
             // btn_pdf
             // 
             this.btn_pdf.Location = new System.Drawing.Point(403, 15);
             this.btn_pdf.Name = "btn_pdf";
-            this.btn_pdf.Size = new System.Drawing.Size(75, 23);
-            this.btn_pdf.TabIndex = 2;
-            this.btn_pdf.Text = "Browse";
+            this.btn_pdf.Size = new System.Drawing.Size(117, 23);
+            this.btn_pdf.TabIndex = 1;
+            this.btn_pdf.Text = "&Passengers Browse";
             this.btn_pdf.UseVisualStyleBackColor = true;
             this.btn_pdf.Click += new System.EventHandler(this.btn_pdf_Click);
             // 
@@ -155,8 +154,8 @@
             this.btn_run.Location = new System.Drawing.Point(188, 180);
             this.btn_run.Name = "btn_run";
             this.btn_run.Size = new System.Drawing.Size(75, 23);
-            this.btn_run.TabIndex = 1;
-            this.btn_run.Text = "Run";
+            this.btn_run.TabIndex = 6;
+            this.btn_run.Text = "&Run";
             this.btn_run.UseVisualStyleBackColor = true;
             this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
             // 
@@ -165,8 +164,8 @@
             this.btn_cancel.Location = new System.Drawing.Point(287, 180);
             this.btn_cancel.Name = "btn_cancel";
             this.btn_cancel.Size = new System.Drawing.Size(75, 23);
-            this.btn_cancel.TabIndex = 2;
-            this.btn_cancel.Text = "Exit";
+            this.btn_cancel.TabIndex = 7;
+            this.btn_cancel.Text = "E&xit";
             this.btn_cancel.UseVisualStyleBackColor = true;
             this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
             // 
@@ -178,8 +177,7 @@
             this.txt_log.ReadOnly = true;
             this.txt_log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.txt_log.Size = new System.Drawing.Size(512, 174);
-            this.txt_log.TabIndex = 3;
-            
+            this.txt_log.TabIndex = 8;
             // 
             // groupBox2
             // 
